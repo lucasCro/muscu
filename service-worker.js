5fabb284aac9919582aac49c4a002e97f24ecc08
lucasCro <66358807+lucasCro@users.noreply.github.com>
2026-09-11 14:02:42 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE = "salle-cache-v1";
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(["./suivi-muscu.html", "./manifest.json", "./icon.svg"]).catch(()=>{}))
+  );
+});
+self.addEventListener("activate", (e) => { self.clients.claim(); });
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(()=>{});
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
